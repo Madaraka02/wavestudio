5e2e7461c5ae212fe22de20e84201010aaeeb262
Madaraka02 <victormadaraka02@gmail.com>
2022-02-24 01:15:31 +0300

    added feedback template

--- archs/models.py
+++ archs/models.py
@@ -2,24 +2,70 @@ from django.db import models
 
 # Create your models here.
 class Partners(models.Model):
-    pass
+    name = models.CharField(max_length=100, )
+    logo = models.FileField(upload_to='logos')
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
-class project(models.Model):
-    pass
+class Project(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    image = models.FileField(upload_to='projimages', null=True)
+    video = models.FileField(upload_to='projvideos', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=True)
 
 
-class Contact(models.Model):
-    pass
+    def __str__ (self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Projects'
+        ordering = ('-id',) 
+
 
 class Feedback(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    testimony = models.TextField()
+    image = models.ImageField(upload_to='images', default="", blank=True, null=True)
+
+
+    def __str__ (self):
+        return self.name    
+
+    class Meta:
+        verbose_name_plural= 'Feedbacks'
+        ordering = ('-id',)       
+
+class Contact(models.Model):
+    name = models.CharField(max_length=300, null=True, blank=True)
+    email = models.CharField(max_length=300)
+    message = models.TextField()
+
+    def __str__ (self):
+        return self.name
+
+    class Meta:
+        ordering = ('-id',) 
 
 class ProjectImages(models.Model):
-    pass
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+    image = models.ImageField(upload_to='projimages', blank=True, null=True) 
+
+    def __str__ (self):
+        return self.project.title
+
+
+    class Meta:
+        verbose_name_plural = 'ProjectImages'
+        ordering = ('-id',)
 
 
