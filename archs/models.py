8c5271407083b01efbf1f7ab144182b94de3b5a9
Madaraka02 <victormadaraka02@gmail.com>
2022-02-23 11:06:08 +0300

    first commit

---
+++ archs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Partners(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class project(models.Model):
+    pass
+
+
+class Contact(models.Model):
+    pass
+
+class Feedback(models.Model):
+    pass
+
+class ProjectImages(models.Model):
+    pass
+
+
