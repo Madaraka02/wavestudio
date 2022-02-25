4a1e6bdb822181ff172515851b1464c77d642fce
Madaraka02 <victormadaraka02@gmail.com>
2022-02-25 10:55:25 +0300

    added admin urls

--- archs/urls.py
+++ archs/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('projects/<int:id>/', projectDeails, name="project"),
     path('contact-us/', contact, name="contact"),
     path('feedback/', feedback, name="feedback"),
+
+   
+    path('wavegridadmin/', admin, name='admin'),
+    path('update/<int:id>/', updateProject, name='update_project'),
+    path('delete-message/<int:id>/', delete_message, name="delete-message"),
+    path('delete-project/<int:id>/', delete_project, name="delete-project"),
+    path('delete-feedback/<int:id>/', delete_feedback, name="delete-feedback"),
 ]
