66694293ac8696c221965debea6480ffd8b6c16d
Madaraka02 <victormadaraka02@gmail.com>
2022-03-02 22:10:25 +0300

    collected static files

--- shucks/settings.py
+++ shucks/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ocsdko9n)z2*cmzx0dj2cz5po*7-qq-m4mciv^i3uyus$uq0x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0',
     'www.wavegridstudio.com.com',
